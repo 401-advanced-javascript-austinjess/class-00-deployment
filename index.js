b0f7b8ea892b0aef95878151bd4d1b153244fecb
Austin Jess <ajess33@gmail.com>
2019-05-20 13:28:42 -0500

    broken code

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ const http = require('http');
  * @param req
  * @param res
  */
-const requestHandler = (req,res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.statusCode = 200;
-  res.write( pol.isAlive().toString() );
-  res.end();
+const requestHandler = (req, res) => {
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 200;
+  // res.write( pol.isAlive().toString() );
+  // res.end();
 };
 
-this should make an error;
-
 const app = http.createServer(requestHandler);
-app.listen(process.env.PORT, () => console.log('server up') );
-
+app.listen(process.env.PORT, () => console.log('server up'));
