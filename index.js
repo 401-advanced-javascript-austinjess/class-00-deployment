bcc9ae06239815df45cacfba0cf58882bb9dfba0
Austin Jess <ajess33@gmail.com>
2019-05-20 13:26:42 -0500

    should break things

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Simple Server
+ * @module index
+ */
+
+const pol = require('./pol.js');
+const http = require('http');
+
+/**
+ * Basic Request Hanlder (All Routes)
+ * @param req
+ * @param res
+ */
+const requestHandler = (req,res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+  res.write( pol.isAlive().toString() );
+  res.end();
+};
+
+this should make an error;
+
+const app = http.createServer(requestHandler);
+app.listen(process.env.PORT, () => console.log('server up') );
+
